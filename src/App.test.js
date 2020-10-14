d02ebcc65ebc33741a3aef69483b683ac8296950
S38629 <s38629@h07169505.lan>
2020-10-14 14:31:57 +0200

    remove failed test

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,3 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('foo error test', () => {
-  expect(true).toBe(false);
-});
