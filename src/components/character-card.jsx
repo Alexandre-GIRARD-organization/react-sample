1da01e1c1d63f56ee81fd8a65d901319012fcaf5
S38629 <s38629@h07169505.lan>
2020-11-14 08:25:14 +0100

    code quality

--- src/components/character-card.jsx
+++ src/components/character-card.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,7 +7,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { getCharacter } from "../services/marvel-characters-service";
 
 const styles = {
   root: {
